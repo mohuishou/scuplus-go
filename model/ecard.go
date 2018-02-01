e3d74a626c2d11e54a1a522f8b40b5afbdd4ac58
mohuishou <1@lailin.xyz>
2018-02-01 21:25:45 +0800

    【一卡通】完善

--- model/ecard.go
+++ model/ecard.go
@@ -1,16 +1,28 @@
 package model
 
 import (
+	"log"
+	"time"
+
 	"github.com/mohuishou/scu/ecard"
 )
 
 // Ecard 一卡通交易数据
 type Ecard struct {
 	Model
-	Time    int64   `json:"time"`    // 交易时间
-	Addr    string  `json:"addr"`    // 交易地点
-	Money   float64 `json:"money"`   // 交易金额
-	Balance float64 `json:"balance"` // 余额
+	TransTime int64   `json:"trans_time"` // 交易时间
+	Addr      string  `json:"addr"`       // 交易地点
+	Money     float64 `json:"money"`      // 交易金额
+	Balance   float64 `json:"balance"`    // 余额
+}
+
+func convertEcard(tran ecard.Transaction) Ecard {
+	return Ecard{
+		TransTime: tran.Time / 1000,
+		Addr:      tran.Addr,
+		Balance:   tran.Balance,
+		Money:     tran.Money,
+	}
 }
 
 // UpdateEcard 更新一卡通信息, 包括更新一卡通余额信息
@@ -21,15 +33,36 @@ func UpdateEcard(uid uint) error {
 		return err
 	}
 
+	// 设置默认开始时间: 2个月内，结束时间: 当日
+	end := time.Now()
+	d, err := time.ParseDuration("60d")
+	log.Println(err)
+	start := end.Add(d)
+
+	// 获取最后一条交易数据
+	lastTrans := Ecard{}
+	DB().Where("user_id = ?", uid).Order("timedesc").Last(&lastTrans)
+
+	if lastTrans.ID != 0 {
+		// 推后一天
+		unix := (lastTrans.TransTime + (3600 * 24))
+		start = time.Unix(unix, unix)
+	}
+
 	// 获取一卡通信息
-	card, err := ecard.Get(c)
+	card, err := ecard.Get(c, start, end)
 
 	// 更新一卡通余额
 	if err := DB().Model(&UserInfo{UserID: uid}).Update("balance", card.Balance).Error; err != nil {
 		return err
 	}
 
-	// 更新交易数据
-
-	return nil
+	// 插入新的交易数据
+	for _, v := range card.Transactions {
+		eCard := convertEcard(v)
+		if err := DB().Create(&eCard).Error; err != nil {
+			log.Printf("[Error]: 更新一卡通数据错误,%s", err.Error())
+			return err
+		}
+	}
 }
