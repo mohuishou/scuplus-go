d1f2998b01f1419d486622fe8f1a8915ba7c6dc2
mohuishou <1@lailin.xyz>
2018-07-14 18:47:43 +0800

    update

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"os"
-
-	"github.com/betacraft/yaag/irisyaag"
-	"github.com/betacraft/yaag/yaag"
-
+	"github.com/kataras/iris/middleware/pprof"
 	"github.com/mohuishou/scuplus-go/config"
 	"github.com/mohuishou/scuplus-go/middleware"
-	"github.com/kataras/iris/middleware/pprof"
 
 	"github.com/kataras/iris"
 	"github.com/mohuishou/scuplus-go/route"
@@ -16,17 +11,6 @@ import (
 
 func main() {
 	app := iris.New()
-
-	env := os.Getenv("SCUPLUS_ENV")
-	if env == "test" {
-		yaag.Init(&yaag.Config{ // <- IMPORTANT, init the middleware.
-			On:       true,
-			DocTitle: "SCUPLUS",
-			DocPath:  "apidoc/apidoc.html",
-			BaseUrls: map[string]string{"Production": "", "Staging": ""},
-		})
-		app.Use(irisyaag.New()) // <- IMPORTANT, register the middleware.
-	}
 
 	// 注册中间件
 	middleware.Register(app)
